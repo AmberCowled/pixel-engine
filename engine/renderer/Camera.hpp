826177812041e6c9cfa670225789e8a3efc90cfa
Amber <amber.cowled97@gmail.com>
2026-05-22 19:21:26 +1000

    Phase 2 Final: Implemented Camera system, fixed winding order, and resolved sync/shutdown leaks

---
+++ engine/renderer/Camera.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace PixelEngine {
+
+    class Camera {
+    public:
+        void SetPerspectiveProjection(float fovy, float aspect, float near, float far);
+        void SetViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up = glm::vec3{0.f, -1.f, 0.f});
+        void SetViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up = glm::vec3{0.f, -1.f, 0.f});
+
+        const glm::mat4& GetProjection() const { return m_ProjectionMatrix; }
+        const glm::mat4& GetView() const { return m_ViewMatrix; }
+
+    private:
+        glm::mat4 m_ProjectionMatrix{1.f};
+        glm::mat4 m_ViewMatrix{1.f};
+    };
+
+}
